c0afae8b9ce913ed19288a5e65018b450d9106e4
wataro <wataro.mudamuda@gmail.com>
2015-02-14 14:55:34 +0900

    generate header file from yaml

---
+++ dev/generate_code.py
@@ -0,0 +1,91 @@
+import argparse
+import yaml
+from os import path
+from pprint import pprint
+
+def get_class(yaml_path):
+    rest, basename = path.split(yaml_path)
+    basename = path.splitext(basename)[0]
+    words = basename.split('_')
+    return ''.join(w.title() for w in words)
+
+def get_include_guard(yaml_path):
+    rest, basename = path.split(yaml_path)
+    rest, dirname1 = path.split(rest)
+    basename = path.splitext(basename)[0].upper()
+    if 'midso' == dirname1:
+        return 'INCLUDE_MIDSO_{}_H_'.format(basename)
+    else:
+        assert 'midso' == path.basename(rest), rest
+        return 'INCLUDE_MIDSO_{}_{}_H_'.format(dirname1, basename)
+
+
+ARG_FORMATTER = '{type} {name}'
+ARG_WITH_DEFAULT_FORMATTER = '{type} {name} = {default}'
+
+MEMBER_FORMATTER = '{args};'
+DECLARE_FUNC_FORMATTER = '    {rettype} {name}({args});'
+DEFINE_FUNC_FORMATTER = '{rettype} {classname}::{name}({args}){\n}'
+
+HEADER_FORMATTER = '''/**
+    Copyright (c) 2015 <Taro WATASUE>
+    
+    This software is released under the MIT License.
+
+    http://opensource.org/licenses/mit-license.php
+*/
+#ifndef {include_guard}
+#define {include_guard}
+
+class {classname} {{
+ public:
+    {classname}();
+
+    ~{classname}();
+
+{declares}
+
+ private:
+    DISALLOW_COPY_AND_ASIGN({classname});
+
+{declares}
+}};
+
+#endif  // {include_guard}
+'''
+
+def get_function_declare(func_dict):
+    key = func_dict.keys()[0]
+    rettype = func_dict[key]['return']
+    assert '()' == key[-2:]
+    name = key[:-2]
+    arg_list = []
+    for arg_dict in func_dict[key].get('args', []):
+        if 'default' in arg_dict:
+            arg = ARG_WITH_DEFAULT_FORMATTER.format(**arg_dict)
+        else:
+            arg = ARG_FORMATTER.format(**arg_dict)
+        arg_list.append(arg)
+    args = '\n        '.join(arg_list)
+    return DECLARE_FUNC_FORMATTER.format(**locals())
+
+
+def get_header_contents(yaml_path):
+    include_guard = get_include_guard(yaml_path)
+    classname = get_class(yaml_path)
+    declare_list = []
+    for func_dict in yaml.load(open(args.yaml_path)):
+        dec = get_function_declare(func_dict)
+        declare_list.append(dec)
+    declares = '\n'.join(declare_list)
+    return HEADER_FORMATTER.format(**locals())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('yaml_path')
+    args = parser.parse_args()
+
+    print get_header_contents(args.yaml_path)
+
+
