4ad8f202ba765f9e3b48b06807af284601824a47
wataro <wataro.mudamuda@gmail.com>
2015-02-19 22:50:22 +0900

    layer is interface

---
+++ test/midso/layer.cpp
@@ -0,0 +1,21 @@
+/**
+    Copyright (c) 2015 <Taro WATASUE>
+    
+    This software is released under the MIT License.
+
+    http://opensource.org/licenses/mit-license.php
+*/
+#include <gtest/gtest.h>
+#include "midso/layer.h"
+
+TEST(Layer, forward) {
+}
+
+TEST(Layer, backward) {
+}
+
+TEST(Layer, out_tensor) {
+}
+
+TEST(Layer, in_diff_tensor) {
+}
---
+++ test/midso/layer_interface.cpp
@@ -0,0 +1,15 @@
+/**
+    Copyright (c) 2015 <Taro WATASUE>
+    
+    This software is released under the MIT License.
+
+    http://opensource.org/licenses/mit-license.php
+*/
+#include <gtest/gtest.h>
+#include "midso/layer_interface.h"
+
+TEST(LayerInterface, propagate) {
+}
+
+TEST(LayerInterface, output_node) {
+}
