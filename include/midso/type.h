fdc05579f389e1c9210ad96b2af623cbfaf55403
wataro <wataro.mudamuda@gmail.com>
2015-02-27 06:44:42 +0900

    Vector is available

--- include/midso/type.h
+++ include/midso/type.h
@@ -3,8 +3,14 @@
 */
 #ifndef INCLUDE_MIDSO_TYPE_H_
 #define INCLUDE_MIDSO_TYPE_H_
+#include <vector>
+
+template<typename T>
+using Vector = std::vector<T>;
 
 typedef std::size_t Size;
+typedef int Int;
+typedef float Float;
 
 #endif  // INCLUDE_MIDSO_TYPE_H_
 
