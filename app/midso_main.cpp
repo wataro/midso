44505fd32ee83530e067f5a296f63371bdd5d731
wataro <wataro.mudamuda@gmail.com>
2015-02-27 06:59:04 +0900

    Pointer

--- app/midso_main.cpp
+++ app/midso_main.cpp
@@ -6,12 +6,16 @@
     http://opensource.org/licenses/mit-license.php
 */
 #include "easylogging/easylogging++.h"
+#include "midso/type.h"
 
 INITIALIZE_EASYLOGGINGPP
+
+using namespace midso;
 
 int main(const int argc, const char ** argv)
 {
     LOG(INFO) << "Make It Deep, Simple, Open!";
+    Pointer<Vector<Int>> p_values;
     return 0;
 }
 
--- include/midso/type.h
+++ include/midso/type.h
@@ -5,12 +5,19 @@
 #define INCLUDE_MIDSO_TYPE_H_
 #include <vector>
 
+namespace midso {
+
 template<typename T>
 using Vector = std::vector<T>;
+
+template<typename T>
+using Pointer = std::unique_ptr<T>;
 
 typedef std::size_t Size;
 typedef int Int;
 typedef float Float;
+
+}  // namespace midso
 
 #endif  // INCLUDE_MIDSO_TYPE_H_
 
