50e3ac113cf465b19f4f8afd0f2e39767b6c22d3
wataro <wataro.mudamuda@gmail.com>
2015-03-07 06:21:23 +0900

    cpplint

---
+++ include/midso/core/linear_algebra.h
@@ -0,0 +1,11 @@
+/**
+    Copyright (c) 2015 <wataro>
+    
+    This software is released under the MIT License.
+
+    http://opensource.org/licenses/mit-license.php
+*/
+#ifndef INCLUDE_MIDSO_CORE_LINEAR_ALGEBRA_H_
+#define INCLUDE_MIDSO_CORE_LINEAR_ALGEBRA_H_
+#endif  // INCLUDE_MIDSO_CORE_LINEAR_ALGEBRA_H_
+
