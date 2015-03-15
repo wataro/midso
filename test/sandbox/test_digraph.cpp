417e48dec6d6af5748518ddb5771ec21867ff1e8
wataro <wataro.mudamuda@gmail.com>
2015-03-15 18:27:49 +0900

    digraph

--- test/sandbox/test_digraph.cpp
+++ test/sandbox/test_digraph.cpp
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
 #include <vector>
-#include <map>
+#include <algorithm>
 
 template<typename T_>
 using Vector = std::vector<T_>;
@@ -8,16 +8,45 @@ using Vector = std::vector<T_>;
 template<typename T1_, typename T2_>
 using Pair = std::pair<T1_, T2_>;
 
-template<typename Key_, typename Value_>
-using Map = std::multimap<Key_, Value_>;
+template<typename T_>
+class Layer
+{
+    public:
+    typedef T_ Value;
+    Layer(const T_ & value): value_(value) {}
+    const T_ & output(void) const { return this->value_; }
+
+    void set_input(const T_ & value)
+    {
+        this->input_values_.push_back(value);
+    }
+    void propagate(void)
+    {
+        for(auto v: this->input_values_) {
+            std::cout << this->value_ << " <- " << v << std::endl;
+        }
+        this->input_values_.clear();
+    }
+    private:
+    Vector<Value> input_values_;
+    Value value_;
+};
 
 template<typename T_>
 class Digraph
 {
     public:
     typedef T_* Item;
-    typedef Pair<T_*, T_*> Arc;
-    typedef Pair<T_*, Vector<Arc>> Vertex;
+    typedef Pair<Item, Item> Arc;
+    typedef Pair<Item, Vector<Item>> Vertex;
+    struct VertexFind {
+        VertexFind(const Item key): key_(key){}
+        Item key_;
+        bool operator()(const Vertex & v)
+        {
+            return this->key_ == v.first;
+        }
+    };
     Digraph(Vector<Arc> & arcs) : arcs_(arcs)
     {
         this->build_dependencies();
@@ -25,8 +54,11 @@ class Digraph
 
     void propagate()
     {
-        for(auto depend: this->dependencies_) {
-            std::cout << depend.first[0] << " <- " << depend.second[0] << std::endl;
+        for(auto vertex: this->vertices_) {
+            for(auto depend: vertex.second) {
+                vertex.first->set_input(depend->output());
+            }
+            vertex.first->propagate();
         }
     }
     private:
@@ -36,13 +68,22 @@ class Digraph
             return;
         }
         Item & root = this->arcs_.front().first;
-        this->dependencies_.insert(std::make_pair(root, root));
+        this->vertices_.push_back(Vertex(root, Vector<Item>(1, root)));
         for(auto arc: this->arcs_) {
-            this->dependencies_.insert(std::make_pair(arc.second, arc.first));
+            auto key = arc.second;
+            auto value = arc.first;
+            auto it = std::find_if(this->vertices_.begin(), this->vertices_.end(), VertexFind(key));
+            if (this->vertices_.end() == it) {
+                this->vertices_.push_back(Vertex(key, Vector<Item>(1, value)));
+            }
+            else
+            {
+                it->second.push_back(value);
+            }
         }
     }
     Vector<Arc> arcs_;
-    Map<Item, Item> dependencies_;
+    Vector<Vertex> vertices_;
 };
 
 TEST(SANDBOX, DIGRAPH)
@@ -52,23 +93,23 @@ TEST(SANDBOX, DIGRAPH)
      *              |                |
      *              +-- [3] ---------+
      * */
-    char vertices[] = {'0', '1', '2', '3', '4', '5'};
-    Digraph<char>::Arc arc_array[] = {
-        std::make_pair(vertices + 0, vertices + 1),
-        std::make_pair(vertices + 1, vertices + 2),
-        std::make_pair(vertices + 1, vertices + 3),
-        std::make_pair(vertices + 2, vertices + 4),
-        std::make_pair(vertices + 3, vertices + 5),
-        std::make_pair(vertices + 4, vertices + 5),
+    Layer<char> layers[] = {'0', '1', '2', '3', '4', '5'};
+    Digraph<Layer<char>>::Arc arc_array[] = {
+        std::make_pair(layers + 0, layers + 1),
+        std::make_pair(layers + 1, layers + 2),
+        std::make_pair(layers + 1, layers + 3),
+        std::make_pair(layers + 2, layers + 4),
+        std::make_pair(layers + 3, layers + 5),
+        std::make_pair(layers + 4, layers + 5),
     };
-    Vector<typename Digraph<char>::Arc> arcs;
+    Vector<typename Digraph<Layer<char>>::Arc> arcs;
     arcs.push_back(arc_array[0]);
     arcs.push_back(arc_array[1]);
     arcs.push_back(arc_array[2]);
     arcs.push_back(arc_array[3]);
     arcs.push_back(arc_array[4]);
     arcs.push_back(arc_array[5]);
-    Digraph<char> digraph(arcs);
+    Digraph<Layer<char>> digraph(arcs);
     digraph.propagate();
 }
 
