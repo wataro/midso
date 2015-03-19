362871e378d725ac93fc60bdfaf3c55dcbc64f1f
wataro <wataro.mudamuda@gmail.com>
2015-03-20 08:03:18 +0900

    add KeyValue

---
+++ src/midso/core/key_value.h
@@ -0,0 +1,27 @@
+/**
+    Copyright (c) 2015 <wataro>
+    
+    This software is released under the MIT License.
+
+    http://opensource.org/licenses/mit-license.php
+*/
+#ifndef INCLUDE_MIDSO_CORE_KEY_VALUE_H_
+#define INCLUDE_MIDSO_CORE_KEY_VALUE_H_
+#include "midso/common.h"
+
+
+namespace midso {
+
+class KeyValue {
+ public:
+    KeyValue() {}
+    ~KeyValue() {}
+
+
+ private:
+    DISALLOW_COPY_AND_ASSIGN(KeyValue);
+};
+
+}  // namespace midso
+
+#endif  // INCLUDE_MIDSO_CORE_KEY_VALUE_H_
